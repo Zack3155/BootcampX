a14bdfa72911658683342b7bf4733d44e2b66240
Zack <zhanbin3155@gmail.com>
2021-11-22 23:51:31 -0500

    BootcampX Queries 4

---
+++ 4_queries/1.sql
@@ -0,0 +1,10 @@
+SELECT
+  COUNT(*) AS total_assistances,
+  teachers.name AS name
+FROM
+  assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+WHERE
+  teachers.name = 'Waylon Boehm'
+GROUP BY
+  name;
